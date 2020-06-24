1510159bd423d96d736c8fa83ec4ccaa0b54fffd
yuanzhijian <408575225@qq.com>
2020-06-25 00:22:31 +0800

    增加登录页面

---
+++ share/proxy/views.py
@@ -0,0 +1,19 @@
+import json
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+from django.views import View
+from .action import FreeProxyHandle
+
+
+class FreeProxy(View):
+    def get(self, request, *args, **kwargs):
+        pass
+
+    def post(self, request, *args, **kwargs):
+        free_proxy_handle = FreeProxyHandle(request)
+        op_handle = free_proxy_handle.get_handle()
+        ret_dict = op_handle()
+        return HttpResponse(json.dumps(ret_dict))
